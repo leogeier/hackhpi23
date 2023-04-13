04f39bab0c10c04f1710d56e007861f3afa25cb0
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-04-13 19:50:51 +0200

    Adds current position and reads in geojson

--- frontend/src/map.jsx
+++ frontend/src/map.jsx
@@ -1,5 +1,74 @@
-import { useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import * as L from "leaflet";
+
+const fallback = [52.39213645214943, 13.123381806173398]
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+
+  let map = useMap();
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng)
+      map.setView(e.latlng);
+    });
+  }, [map]);
+
+  return (
+    <Marker position={position ?? fallback}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+
+function LoadStreets() {
+
+  const example_json = `{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "stroke": "#ff2d28",
+          "stroke-width": 2,
+          "stroke-opacity": 1
+        },
+        "geometry": {
+          "coordinates": [
+            [8.910232129914675, 50.14536687853564],
+            [8.911144597334612, 50.14742157191719],
+            [8.911524792092678, 50.14826616335989]
+          ],
+          "type": "LineString"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "stroke": "#10ff00",
+          "stroke-width": 2,
+          "stroke-opacity": 1
+        },
+        "geometry": {
+          "coordinates": [
+            [8.908811602572115, 50.14699078160584],
+            [8.910979915141581, 50.1469691551643]
+          ],
+          "type": "LineString"
+        },
+        "id": 1
+      }
+    ]
+  }`;
+  
+  const example_object = JSON.parse(example_json);
+  const map = useMap();
+  L.geoJSON(example_object).addTo(map);
+  
+  return null;
+}
 
 function InvalidateOnLoad() {
   const map = useMap();
@@ -9,20 +78,23 @@ function InvalidateOnLoad() {
 }
 
 export default function Map() {
+  
+
   return (
     <div id='map' className='w-screen h-96'>
-      <MapContainer center={[40.505, -100.09]} zoom={13} className='w-full h-full' >
-      <InvalidateOnLoad/>
+      <MapContainer center={fallback} zoom={13} className='w-full h-full' >
+        <InvalidateOnLoad/>
+        <LocationMarker />
+        <LoadStreets/>
+
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
-        <Marker position={[40.505, -100.09]}>
-              <Popup>
-                I am a pop-up!
-              </Popup>
-          </Marker>
+        <TileLayer
+          attribution='&copy; <a href="https://www.flaticon.com/de/kostenlose-icons/zigarre" title="zigarre Icons">Zigarre Icons erstellt von Freepik - Flaticon</a>'
+          url="https://www.flaticon.com/de/kostenlose-icons/zigarre"
+        />
       </MapContainer>
  </div>
   );
