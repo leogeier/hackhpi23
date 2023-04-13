60c07b9a537f4fc2b3676f4fe993d6eebbe3b629
leogeier <leonard.geier@hotmail.com>
2023-04-13 16:50:54 +0200

    frontend setup

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import Main from './main.jsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Main />);
---
+++ frontend/src/main.jsx
@@ -0,0 +1,3 @@
+export default function Main() {
+  return (<h1 className="text-3xl font-bold underline">Hello</h1>);
+}
